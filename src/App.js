9158243354b3bc84b1042cc48599d1061868d751
arifvtti <arif.vtti@gmail.com>
2022-07-04 12:30:02 +0600

    navbar added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
 import Header from './pages/shared/Header';
+import Navbar from './pages/shared/Navbar';
 
 function App() {
   return (
-    <div className="App">
+    <div className="max-w-7xl mx-auto px-2">
       <Header/>
+      <Navbar>
+
+      </Navbar>
     </div>
   );
 }
